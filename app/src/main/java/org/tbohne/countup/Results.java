7739efc6bc97e9279bad7136823914eb744d523c
tbohne <tbohne@uos.de>
2018-09-28 10:57:58 +0200

    added basic results class

---
+++ app/src/main/java/org/tbohne/countup/Results.java
@@ -0,0 +1,19 @@
+package org.tbohne.countup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Results extends AppCompatActivity {
+
+    /**
+     * Called when the activity is starting.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results);
+    }
+
+}
