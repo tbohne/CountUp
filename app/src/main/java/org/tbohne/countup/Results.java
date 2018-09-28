7ea18a44e1122e914505da1269de589f9ce4bda5
tbohne <tbohne@uos.de>
2018-09-28 11:59:27 +0200

    implemented first version of results activity

--- app/src/main/java/org/tbohne/countup/Results.java
+++ app/src/main/java/org/tbohne/countup/Results.java
@@ -2,8 +2,16 @@ package org.tbohne.countup;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Results extends AppCompatActivity {
+
+    private ArrayList<String> activities;
+    private ArrayList<String> times;
+    private ArrayList<TextView> activityViews;
+    private ArrayList<TextView> activityTimeViews;
 
     /**
      * Called when the activity is starting.
@@ -14,6 +22,43 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+
+        Bundle bundle = getIntent().getExtras();
+        this.activities = bundle.getStringArrayList("activities");
+        this.times = bundle.getStringArrayList("times");
+
+        this.activityViews = new ArrayList<>();
+        this.activityViews.add((TextView) findViewById(R.id.act0));
+        this.activityViews.add((TextView) findViewById(R.id.act1));
+        this.activityViews.add((TextView) findViewById(R.id.act2));
+        this.activityViews.add((TextView) findViewById(R.id.act3));
+        this.activityViews.add((TextView) findViewById(R.id.act4));
+        this.activityViews.add((TextView) findViewById(R.id.act5));
+
+        this.setActivities();
+
+        this.activityTimeViews = new ArrayList<>();
+        this.activityTimeViews.add((TextView) findViewById(R.id.act0_time));
+        this.activityTimeViews.add((TextView) findViewById(R.id.act1_time));
+        this.activityTimeViews.add((TextView) findViewById(R.id.act2_time));
+        this.activityTimeViews.add((TextView) findViewById(R.id.act3_time));
+        this.activityTimeViews.add((TextView) findViewById(R.id.act4_time));
+        this.activityTimeViews.add((TextView) findViewById(R.id.act5_time));
+
+        this.setTimes();
+    }
+
+    private void setActivities() {
+        for (int i = 0; i < this.activityViews.size(); i++) {
+            this.activityViews.get(i).setText(this.activities.get(i));
+        }
+    }
+
+    private void setTimes() {
+        for (int i = 0; i < this.activityTimeViews.size(); i++) {
+            TextView currentView = this.activityTimeViews.get(i);
+            currentView.setText(this.times.get(i) + "");
+        }
     }
 
 }
